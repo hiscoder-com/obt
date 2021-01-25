2155483b50e5e07410d029187d648fe138ce425f
Azedval <azedval@gmail.com>
2021-01-26 02:48:53 +0300

    first commit

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,20 +6,16 @@ import {
 
 function BookList ({onBookId}) {
   const {state} = React.useContext(ResourcesContext);
-  if (state)
-  {
-    debugger;
-  }
-
-  const bookList = (state && state.resources && state.resources[0] && state.resources[0].projects)? 
-      state.resources[0].projects.map(
-        b => <p style={{color: 'black'}}><a onClick={() => onBookId(b.identifier)}>{b.title}</a></p>
-      ) : [];
-
+  
+  const bookListOld = (state && state.resources && state.resources[0] && state.resources[0].projects ) ? 
+    state.resources[0].projects.filter(itm => itm.categories[0]==='bible-ot').map(itm => <p style={{color: 'blue', marginLeft:'300px'}}> {itm.title} </p>) : [];
+  const bookListNew = (state && state.resources && state.resources[0] && state.resources[0].projects ) ? 
+    state.resources[0].projects.filter(itm => itm.categories[0]==='bible-nt').map(itm => <p style={{color: 'blue',marginLeft:'30px'}}> {itm.title} </p>) : [];
   return (
-    <>
-    {bookList}
-    </>
+    <p>
+  <h2 style={{color: '#49423d',marginLeft:'300px'}}><a onClick ={() => onBookId(bookListOld)}   >Old Testament</a></h2>
+  <h2 style={{color: '#49423d',marginLeft:'30px'}}><a onClick ={() => onBookId(bookListNew)}   >New Testament</a></h2>
+    </p>
   );
 }
 
