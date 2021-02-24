ce616f7b8d0c264b3bb407058da3ae19612904df
dimanya <dimanya35@gmail.com>
2021-02-24 21:58:33 +0500

    modal window

--- src/components/TypoReport/index.js
+++ src/components/TypoReport/index.js
@@ -2,35 +2,47 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import { Button, Container } from '@material-ui/core';
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     top: 'auto',
     bottom: 0,
   },
+  select: {
+    color: 'red',
+  },
 }));
 
 export default function BottomAppBar() {
   const [open, setOpen] = React.useState(false);
 
-  const handleClick = () => {
-    const url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSeoo8oL77CpI9Axy2uT6zTCVH9wvZNmvyDaOft6d_-ZOzlIpQ/formResponse?entry.2013941061=${window
-      .getSelection()
-      .toString()}&entry.1397183109=${window.location}`;
+  const selectionNode1 = window.getSelection();
 
-    fetch(url);
-    setOpen(true);
-  };
+  console.log(selectionNode1);
 
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
+  const selectionNode = window
+    .getSelection()
+    ?.anchorNode?.parentNode?.textContent?.toString();
+
+  const selection = window.getSelection().toString();
+
+  function handleClickOpen() {
+    if (window.getSelection().toString()) {
+      return setOpen(true);
     }
+  }
 
+  const handleClose = () => {
     setOpen(false);
   };
 
@@ -39,24 +51,78 @@ export default function BottomAppBar() {
   return (
     <AppBar position="fixed" color="primary" className={classes.appBar}>
       <Container fixed>
-        <Typography variant="h6">
+        <Typography variant="subtitle1">
           Если вы нашли ошибку, то выделите этот фрагмент текста и нажмите &nbsp;
-          <Button color="secondary" variant="contained" onClick={handleClick}>
+          <Button color="inherit" variant="contained" onClick={handleClickOpen}>
             Сообщить об ошибке
           </Button>
           &nbsp; Спасибо!
-          <Snackbar
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Сообщить об опечатке</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Текст, который будет отправлен нашим редакторам:
+              </DialogContentText>
+              <DialogContentText className={classes.select}>
+                {selection}
+              </DialogContentText>
+              <DialogContentText>{selectionNode}</DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="comment"
+                label="Ваш комментарий (не обязательно):"
+                type="text"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Отправить
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Typography>
+      </Container>
+    </AppBar>
+  );
+}
+
+/**/
+
+/*
+    const url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSeoo8oL77CpI9Axy2uT6zTCVH9wvZNmvyDaOft6d_-ZOzlIpQ/formResponse?entry.2013941061=${window
+      .getSelection()
+      .toString()}&entry.1397183109=${window.location}`;
+
+    fetch(url, {
+      mode: 'no-cors',
+      method: 'GET',
+      headers: { 'Access-Control-Allow-Origin': 'http://localhost:3000/' },
+      credentials: 'include',
+    });
+    setOpen(true);
+    
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };*/
+
+/*<Snackbar
             anchorOrigin={{
               vertical: 'bottom',
               horizontal: 'center',
             }}
             open={open}
-            autoHideDuration={6000}
+            autoHideDuration={10000}
             onClose={handleClose}
             message="Ваша заметка отправлена"
             action={
               <React.Fragment>
-                <Button color="secondary" size="small" onClick={handleClose} />
                 <IconButton
                   size="small"
                   aria-label="close"
@@ -67,9 +133,8 @@ export default function BottomAppBar() {
                 </IconButton>
               </React.Fragment>
             }
-          />
-        </Typography>
-      </Container>
-    </AppBar>
-  );
-}
+          />*/
+
+/*import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';*/
