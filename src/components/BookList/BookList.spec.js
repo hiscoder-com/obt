b31e07b32ac7127fa57c5c09d2ae565351788768
ancientTexts-net <zach.pennington@unfoldingWord.org>
2021-04-06 10:47:42 -0500

    Sample unit tests

--- src/components/BookList/BookList.spec.js
+++ src/components/BookList/BookList.spec.js
@@ -2,7 +2,7 @@ import BookList from './BookList';
 
 describe('Checking BookList - TODO TEST', () => {
   it('should test something', () => {
-    console.log("TEST!")
-    expect(true).toBe(true);
+    console.log("TEST!");
+    expect(false).toBe(true);
   })
 })
