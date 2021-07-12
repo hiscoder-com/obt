fb4e706d1b9b2694e08105752b42c68ec1ebd559
Vorobushec <68908261+Vorobushec@users.noreply.github.com>
2021-07-12 17:09:51 +0300

    switching books to short cut

--- src/KeyboardShortcut.js
+++ src/KeyboardShortcut.js
@@ -1,17 +1,29 @@
 import { useContext } from 'react';
-import { getBookChapters } from '@texttree/tt-reference-rcl';
+import { getBookChapters, getAllChapters } from '@texttree/tt-reference-rcl';
 import { AppContext } from './App.context';
 
 export default function Shortcut() {
   const { state, actions } = useContext(AppContext);
   const { referenceSelected } = state;
   const { setReferenceSelected } = actions;
-
   document.onkeyup = (event) => {
     let chapterNumber = parseInt(referenceSelected.chapter);
-    let Chapters = Object.keys(getBookChapters(referenceSelected.bookId));
+    let chapters = Object.keys(getBookChapters(referenceSelected.bookId));
+    let allBooks = Object.keys(getAllChapters());
+    let openBook = allBooks.indexOf(referenceSelected.bookId);
+    let pastBookIndex = openBook - 1;
+    let nextBookIndex = openBook + 1;
+    let pastBook = allBooks[pastBookIndex];
+    let nextBook = allBooks[nextBookIndex];
+    console.log(referenceSelected);
     if (event.code === 'ArrowRight' && event.ctrlKey) {
-      if (chapterNumber !== Chapters.length) {
+      if (referenceSelected.bookId === 'rev' && chapterNumber === chapters.length) {
+        setReferenceSelected({
+          bookId: 'rev',
+          chapter: String(chapters.length),
+          verse: 1,
+        });
+      } else if (chapterNumber !== chapters.length) {
         chapterNumber++;
         setReferenceSelected({
           ...referenceSelected,
@@ -19,20 +31,34 @@ export default function Shortcut() {
           verse: 1,
         });
       } else {
-        chapterNumber = Chapters.length;
-      }
-    } else if (event.code === 'ArrowLeft' && event.ctrlKey) {
-      if (chapterNumber === 1) {
-        console.log(Chapters[0]);
-        chapterNumber = 1;
-      } else {
-        console.log(Chapters[0]);
-        chapterNumber--;
         setReferenceSelected({
-          ...referenceSelected,
-          chapter: String(chapterNumber),
+          bookId: nextBook,
+          chapter: '1',
           verse: 1,
         });
+      }
+    } else if (event.code === 'ArrowLeft' && event.ctrlKey) {
+      if (referenceSelected.bookId === 'gen' && chapterNumber === 1) {
+        setReferenceSelected({
+          bookId: 'gen',
+          chapter: '1',
+          verse: 1,
+        });
+      } else {
+        if (chapterNumber === 1) {
+          setReferenceSelected({
+            bookId: pastBook,
+            chapter: String(chapters.length),
+            verse: 1,
+          });
+        } else {
+          chapterNumber--;
+          setReferenceSelected({
+            ...referenceSelected,
+            chapter: String(chapterNumber),
+            verse: 1,
+          });
+        }
       }
     }
   };
