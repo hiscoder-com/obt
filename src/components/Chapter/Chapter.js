21d597e5284619c70fbcdde5dd76fdcf4d94d12c
User Fox <foxprogs@gmail.com>
2021-08-27 14:43:22 +0400

    WIP

--- src/components/Chapter/Chapter.js
+++ src/components/Chapter/Chapter.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
-import { Card } from 'translation-helps-rcl';
-import { Verse } from 'scripture-resources-rcl';
+import { Card, useContent } from 'translation-helps-rcl';
 
 import USFMContent from './USFMContent';
 import VerseMenu from './VerseMenu';
 import { AppContext, ReferenceContext } from '../../context';
-import { getVerseText } from '../../helper';
+
+import { server } from '../../config/base';
 
 const initialPosition = {
   mouseX: null,
@@ -14,27 +14,18 @@ const initialPosition = {
 };
 
 export default function Chapter({ title, classes, onClose, type, reference }) {
-  const verseRef = useRef([]);
-  const [position, setPosition] = React.useState(initialPosition);
+  const [position, setPosition] = useState(initialPosition);
   const {
     state: { resourcesApp, fontSize },
   } = useContext(AppContext);
 
   const {
-    actions: { goToBookChapterVerse, setReferenceBlock },
+    state: {
+      referenceSelected: { bookId, chapter },
+    },
   } = useContext(ReferenceContext);
 
-  const [chapter, setChapter] = useState();
-  const [verses, setVerses] = useState();
   const [resource, setResource] = useState(false);
-
-  const handleContextOpen = (event) => {
-    event.preventDefault();
-    setPosition({
-      mouseX: event.clientX - 2,
-      mouseY: event.clientY - 4,
-    });
-  };
 
   useEffect(() => {
     resourcesApp.forEach((el) => {
@@ -44,64 +35,15 @@ export default function Chapter({ title, classes, onClose, type, reference }) {
     });
   }, [resourcesApp, type]);
 
-  useEffect(() => {
-    if (chapter && verseRef.current[reference.verse]) {
-      verseRef.current[reference.verse].scrollIntoView({
-        block: 'center',
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [reference.verse]);
-
-  useEffect(() => {
-    let _verses = [];
-    for (let key in chapter) {
-      if (parseInt(key).toString() !== key.toString()) {
-        continue;
-      }
-      const { verseObjects } = chapter[key];
-      const verseStyle = {
-        fontSize: fontSize + '%',
-        cursor: 'context-menu',
-        fontWeight: key === reference.verse ? 'bold' : 'inherit',
-      };
-      const verse = (
-        <div
-          ref={(ref) => (verseRef.current[key] = ref)}
-          style={verseStyle}
-          className={'verse' + (key === reference.verse ? ' current' : '')}
-          key={key}
-          onContextMenu={(e) => {
-            e.preventDefault();
-            setReferenceBlock({
-              ...reference,
-              resource: type,
-              verse: key,
-              text: getVerseText(verseObjects),
-            });
-            handleContextOpen(e);
-          }}
-          onClick={() =>
-            reference.verse !== key
-              ? goToBookChapterVerse(reference.bookId, reference.chapter, key)
-              : false
-          }
-        >
-          <Verse
-            verseKey={key}
-            verseObjects={verseObjects}
-            paragraphs={false}
-            showUnsupported={false}
-            disableWordPopover={false}
-            reference={{ ...reference, verse: key }}
-            renderOffscreen={false}
-          />
-        </div>
-      );
-      _verses.push(verse);
-    }
-    setVerses(_verses);
-  }, [chapter, reference, type, setReferenceBlock, goToBookChapterVerse, fontSize]);
+  const content = useContent({
+    chapter: chapter,
+    projectId: bookId,
+    branch: resource.branch ?? 'master',
+    languageId: resource.languageId ?? 'ru',
+    resourceId: type.split('_')[1] ?? 'rob',
+    owner: resource.owner ?? 'door43-catalog',
+    server,
+  });
 
   return (
     <Card
@@ -110,6 +52,7 @@ export default function Chapter({ title, classes, onClose, type, reference }) {
       title={title}
       type={type}
       classes={classes}
+      fontSize={fontSize}
     >
       <VerseMenu
         position={position}
@@ -117,10 +60,9 @@ export default function Chapter({ title, classes, onClose, type, reference }) {
         initialPosition={initialPosition}
       />
       <USFMContent
-        chapter={chapter}
-        setChapter={setChapter}
-        resource={resource}
-        verses={verses}
+        setPosition={setPosition}
+        content={content}
+        type={type}
         reference={reference}
       />
     </Card>
--- src/components/Chapter/USFMContent.js
+++ src/components/Chapter/USFMContent.js
@@ -1,32 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { Verse } from 'scripture-resources-rcl';
+import { getVerseText } from '../../helper';
+import { ReferenceContext } from '../../context';
 import { useTranslation } from 'react-i18next';
-import { ResourcesContext } from 'scripture-resources-rcl';
+import { useScrollToVerse } from '../../hooks/useScrollToVerse';
 
-function USFMContent({ chapter, verses, setChapter, resource, reference }) {
+function USFMContent({ reference, setPosition, content, type }) {
   const { t } = useTranslation();
-  const { state } = React.useContext(ResourcesContext);
-  const [project, setProject] = useState({});
+  const [verses, setVerses] = useState();
+  const [chapter, setChapter] = useState();
+  const [verseRef] = useScrollToVerse('center');
 
-  const resources = state?.resources;
+  const {
+    actions: { setReferenceBlock, goToBookChapterVerse },
+  } = React.useContext(ReferenceContext);
+  const resource = content?.resource;
   useEffect(() => {
-    if (resources) {
-      resources.forEach((el) => {
-        if (
-          el.repository === resource.name &&
-          el.username.toString().toLowerCase() === resource.owner.toString().toLowerCase()
-        ) {
-          setProject(el.project);
-        }
-      });
-    }
-  }, [resources, resource]);
-
-  useEffect(() => {
-    if (project && Object.keys(project).length !== 0) {
-      project
+    if (resource?.project && Object.keys(resource.project).length !== 0) {
+      resource.project
         .parseUsfm()
         .then((result) => {
-          if (result.json && Object.keys(result.json.chapters).length > 0) {
+          console.log({ result: result });
+          if (Object.keys(result.json.chapters).length > 0) {
             setChapter(result.json.chapters[reference.chapter]);
           }
         })
@@ -35,9 +30,63 @@ function USFMContent({ chapter, verses, setChapter, resource, reference }) {
       setChapter(null);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [project, reference.chapter]);
+  }, [resource, reference.chapter]);
 
-  return <div>{chapter ? verses : t('No_content')}</div>;
+  useEffect(() => {
+    const handleContextMenu = (e, key, verseObjects) => {
+      e.preventDefault();
+      setReferenceBlock({
+        ...reference,
+        resource: type,
+        verse: key,
+        text: getVerseText(verseObjects),
+      });
+      setPosition({
+        mouseX: e.clientX - 2,
+        mouseY: e.clientY - 4,
+      });
+    };
+
+    let _verses = [];
+    for (let key in chapter) {
+      if (parseInt(key).toString() !== key.toString()) {
+        continue;
+      }
+      const { verseObjects } = chapter[key];
+      const handleClick = () =>
+        reference.verse !== key
+          ? goToBookChapterVerse(reference.bookId, reference.chapter, key)
+          : false;
+
+      const verse = (
+        <div
+          ref={(ref) => {
+            key === reference.verse && verseRef(ref);
+          }}
+          className={'verse' + (key === reference.verse ? ' current' : '')}
+          key={key}
+          onContextMenu={(e) => handleContextMenu(e, key, verseObjects)}
+          onClick={handleClick}
+        >
+          <Verse
+            verseKey={key}
+            verseObjects={verseObjects}
+            paragraphs={false}
+            showUnsupported={false}
+            disableWordPopover={false}
+            reference={{ ...reference, verse: key }}
+            renderOffscreen={false}
+          />
+        </div>
+      );
+
+      _verses.push(verse);
+    }
+    setVerses(_verses);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chapter, reference, type]);
+
+  return <>{chapter ? verses : t('No_content')}</>;
 }
 
 export default USFMContent;
