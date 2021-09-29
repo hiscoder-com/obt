de309f5cf68897a1f0c265e761c9ff4bdc161f4d
valyukhov <alexzed@bk.ru>
2021-09-29 18:11:29 +0300

    parse .md

---
+++ src/CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.1.0] - 2021-09-22
+
+### Added
+- Swipes (left-right) to switch chapter on the phone
+- Current reference(book,chapter,verse) of Bible/OBS in app bar: clickable and adaptive for screen size
+- Select Bible/OBS mode in app bar
+- Service Worker for caching requests
+
+### Changed (for changes in existing functionality)
+- In main menu removed options for change Bible/OBS mode
+- UI version of buttons in app bar
+- Font style in title of cards
+- Updated bible-reference-rcl version 1.1.0, added OBS support, save filter after updating the list of books
+- Bundle optimization
+- Lazy loading
+
+### Fixed
+- Can't perform a React state update on an unmounted component
+- Switch OBS to Bible when open user guide
+- If the content did not fit on the screen, then when scrolling to a verse, the main screen scrolled
+
+### Security
+- Updating dependencies
+
+## [1.0.0] - 2021-09-02
+
+- First release
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import * as PACKAGE_JSON from '../../../package.json';
+
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import ReactMarkdown from 'react-markdown';
+
+import changeLog from '../../CHANGELOG.md';
+import * as PACKAGE_JSON from '../../../package.json';
 
 function About() {
-  console.log(PACKAGE_JSON);
+  const [log, setLog] = React.useState();
 
+  React.useEffect(() => {
+    fetch(changeLog)
+      .then((response) => response.text())
+      .then((text) => {
+        setLog({ text: text });
+      });
+  }, []);
+  console.log(log);
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -31,7 +43,9 @@ function About() {
       >
         <DialogTitle id="alert-dialog-title">{'About'}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">ChangeLog</DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            <ReactMarkdown>{log.text}</ReactMarkdown>
+          </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary" autoFocus>
