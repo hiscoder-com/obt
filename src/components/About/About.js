10de0144e77e28f04919208057e8be6759a32f55
valyukhov <alexzed@bk.ru>
2021-10-01 09:42:15 +0300

    clean

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import ReactMarkdown from 'react-markdown';
 
 import {
   Button,
@@ -11,16 +13,13 @@ import {
   MenuItem,
 } from '@material-ui/core';
 
-import ReactMarkdown from 'react-markdown';
-
 import changeLog from '../../docs/CHANGELOG.md';
-
 import * as PACKAGE_JSON from '../../../package.json';
 
 function About({ open, setOpen, handleClick }) {
-  const [log, setLog] = React.useState();
-  console.log(log);
-  React.useEffect(() => {
+  const [log, setLog] = useState();
+
+  useEffect(() => {
     fetch(changeLog)
       .then((response) => response.text())
       .then((text) => {
