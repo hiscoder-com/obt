be81034cce2a7855c5b718e7e0b9466ce94a5e5b
Valyukhov <alexzed@bk.ru>
2021-10-12 08:25:27 +0300

    New version of update

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import useStyles from './style';
 //const Card = React.lazy(() => import('./components/Card/Card'));
 //const TypoReport = React.lazy(() => import('./components/TypoReport/TypoReport'));
 //const SubMenuBar = React.lazy(() => import('./components/SubMenuBar/SubMenuBar'));
-import { updateVer1_3_1 } from './updateVersion';
+import { updateVer1_3_0 } from './updateVersion';
 
-updateVer1_3_1('appConfig');
+updateVer1_3_0('appConfig');
 export default function App() {
   const {
     state: { appConfig, resourcesApp, resources, breakpoint },
