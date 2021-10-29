2b1412366a5490137520dc279042a7b51e5a059e
Vitek Foxprogs <foxprogs@gmail.com>
2021-10-29 21:10:03 +0400

    move styles

--- src/components/StartDialog/StartDialog.js
+++ src/components/StartDialog/StartDialog.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SelectLanguage, SelectResourcesLanguages, DialogUI } from '../../components/';
 
@@ -10,34 +10,23 @@ import {
   StepLabel,
   StepContent,
   Typography,
-  makeStyles,
 } from '@material-ui/core';
 
 import Base from './Base';
 import { AppContext, ReferenceContext } from '../../context';
 
+import { useStyles } from './style';
 import { resetWorkspace } from '../../helper';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  backButton: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-  divider: { marginTop: theme.spacing(2), marginBottom: theme.spacing(2) },
-}));
 
 function StartDialog() {
   const { t } = useTranslation();
+  const classes = useStyles();
+
   const {
     actions: { setOpenStartDialog, setLoadIntro, setAppConfig },
     state: { openStartDialog, currentLanguage },
   } = useContext(AppContext);
+
   const {
     state: {
       referenceSelected: { bookId },
@@ -45,13 +34,16 @@ function StartDialog() {
     actions: { goToBookChapterVerse },
   } = useContext(ReferenceContext);
 
+  const [activeStep, setActiveStep] = useState(0);
+  const [openDialog, setOpenDialog] = useState(openStartDialog);
+
   const handleClose = () => {
     setOpenStartDialog(false);
     setOpenDialog(false);
     setLoadIntro(true);
   };
-  const [openDialog, setOpenDialog] = React.useState(openStartDialog);
-  function getStepContent(stepIndex) {
+
+  const getStepContent = (stepIndex) => {
     switch (stepIndex) {
       case 0:
         return <SelectLanguage style={{ maxWidth: '250px' }} />;
@@ -60,10 +52,7 @@ function StartDialog() {
       default:
         return '';
     }
-  }
-
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
+  };
 
   const steps = [
     { label: t('Interface_lang'), instruction: t('Choose_language') },
@@ -72,7 +61,8 @@ function StartDialog() {
       instruction: t('Choose_languages_resources'),
     },
   ];
-  React.useEffect(() => {
+
+  useEffect(() => {
     if (activeStep > steps.length - 1) {
       resetWorkspace({
         bookId,
@@ -84,7 +74,8 @@ function StartDialog() {
       handleClose();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeStep, setOpenStartDialog, steps.length]);
+  }, [activeStep, steps.length]);
+
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
@@ -96,19 +87,18 @@ function StartDialog() {
   return (
     <DialogUI open={openDialog} onClose={handleClose}>
       <Base>
-        <div style={{ textAlign: 'center' }}>
+        <div className={classes.positionCenter}>
           {activeStep < steps.length && (
             <Typography variant="h5">{steps[activeStep].label}</Typography>
           )}
         </div>
         <Divider className={classes.divider} />
-        <div className={classes.root} style={{ textAlign: 'center' }}>
+        <div className={classes.root}>
           <Stepper
             activeStep={activeStep}
-            style={{ padding: '20px 6px' }}
+            className={classes.myStepper}
             orientation={'vertical'}
           >
-            {/** TODO i tried to remove stepper to other component, but i can't raised state to up  */}
             {steps.map((step) => (
               <Step key={step.label}>
                 <StepLabel>{step.instruction}</StepLabel>
@@ -117,7 +107,7 @@ function StartDialog() {
             ))}
           </Stepper>
           <Divider className={classes.divider} />
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <div className={classes.buttonBlock}>
             <Button
               onClick={activeStep === 0 ? handleClose : handleBack}
               className={classes.backButton}
