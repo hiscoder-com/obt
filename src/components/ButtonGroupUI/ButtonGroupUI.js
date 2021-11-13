1eb73e9d71e68968e8216849a5243a3e473f3134
Vitek Foxprogs <foxprogs@gmail.com>
2021-11-13 10:06:11 +0400

    change in showReference

--- src/components/ButtonGroupUI/ButtonGroupUI.js
+++ src/components/ButtonGroupUI/ButtonGroupUI.js
@@ -3,13 +3,13 @@ import React from 'react';
 import useStyles, { useButtonStyles } from './style';
 import { ButtonGroup, Button } from '@material-ui/core';
 
-export default function ButtonGroupUI({ buttons }) {
+export default function ButtonGroupUI({ buttons = [], buttonGroupProps = {} }) {
   const classes = useStyles();
   const classesButton = useButtonStyles();
 
   return (
     <div className={classes.root}>
-      <ButtonGroup>
+      <ButtonGroup {...buttonGroupProps}>
         {buttons.map((el, index) => (
           <Button key={index} className={classesButton.root} onClick={el.onClick}>
             {el.title}
