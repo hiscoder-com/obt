ca3feeb8b0fab32879af76d186bc34559850e449
Vitek Foxprogs <foxprogs@gmail.com>
2021-11-13 09:46:23 +0400

    add universal method

--- src/components/ButtonGroupUI/ButtonGroupUI.js
+++ src/components/ButtonGroupUI/ButtonGroupUI.js
@@ -3,24 +3,18 @@ import React from 'react';
 import useStyles, { useButtonStyles } from './style';
 import { ButtonGroup, Button } from '@material-ui/core';
 
-export default function ButtonGroupUI({
-  onFirstButtonClick,
-  onSecondButtonClick,
-  titleFirst,
-  titleSecond,
-}) {
+export default function ButtonGroupUI({ buttons }) {
   const classes = useStyles();
   const classesButton = useButtonStyles();
 
   return (
     <div className={classes.root}>
-      <ButtonGroup aria-label="outlined primary button group">
-        <Button className={classesButton.root} onClick={onFirstButtonClick}>
-          {titleFirst}
-        </Button>
-        <Button className={classesButton.root} onClick={onSecondButtonClick}>
-          {titleSecond}
-        </Button>
+      <ButtonGroup>
+        {buttons.map((el, index) => (
+          <Button key={index} className={classesButton.root} onClick={el.onClick}>
+            {el.title}
+          </Button>
+        ))}
       </ButtonGroup>
     </div>
   );
