05cbecd38419785b9fdd1d223bed2aa991c7afa0
Vorobushec <timur.yancev08@gmail.com>
2022-03-09 10:48:39 +0400

    WIP

--- src/components/CopyLayout/CopyLayout.js
+++ src/components/CopyLayout/CopyLayout.js
@@ -1,40 +1,65 @@
-import React, { useContext, useState } from 'react';
-import { encode, decode } from 'js-base64';
-
-import { useSnackbar } from 'notistack';
-import { useTranslation } from 'react-i18next';
-
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../../context';
-
-import { Button } from '@material-ui/core';
+import { Button, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 
 export default function CopyLayout() {
-  const { t } = useTranslation();
-  const { enqueueSnackbar } = useSnackbar();
-
   const {
     state: { appConfig, saveLayout },
-    actions: { setSaveLayout, setAppConfig },
+    actions: { setAppConfig, setSaveLayout },
   } = useContext(AppContext);
+  // const [saveLayout, setSaveLayout] = useState();
   const [stValue, setStValue] = useState(JSON.stringify(appConfig));
-
+  const [nameLayout, setNameLayout] = useState('Название лэяута');
+  const [arr, setArr] = useState([]);
+  const layout = {
+    name: nameLayout,
+    value: stValue,
+  };
+  // console.log(layout);
+  console.log(arr);
+  console.log(saveLayout);
   const CopyLayout = () => {
-    setSaveLayout(stValue);
+    setSaveLayout(layout);
   };
-  console.log(saveLayout, 'новый лаяут');
   const getLayout = () => {
-    setAppConfig(JSON.parse(saveLayout));
+    setAppConfig(JSON.parse(saveLayout.value));
   };
-
   const onClick = (event) => {
     setStValue(event.target.value);
   };
-
+  const onClicks = (event) => {
+    setNameLayout(event.target.value);
+  };
+  const result = arr.map((element, index) => {
+    return (
+      <button key={index} onClick={getLayout} onDoubleClick={() => remove(index)}>
+        {element.name}
+      </button>
+    );
+  });
+  function remove(index) {
+    setArr([...arr.slice(0, index), ...arr.slice(index + 1)]);
+  }
+  // return (
+  //   <div>
+  //     {result}
+  //     {/* <button value={value} onChange={(event) => setValue(event.target.value)} /> */}
+  //     <button onClick={() => setArr([...arr, value])}>Добавить элемент</button>
+  //   </div>
+  // );
   return (
     <>
-      <form name="test" method="post" action="input1.php">
+      <FormControl>
+        <InputLabel shrink id="themeId">
+          Layouts
+        </InputLabel>
+        <Select disableUnderline={true} labelId="themeId">
+          {<MenuItem>{result}</MenuItem>}
+        </Select>
+      </FormControl>
+      <form name="test" method="post">
         <p>
-          <input type="text" placeholder="название сохраняемого layouta" size="40" />
+          <input type="text" onChange={onClicks} placeholder={nameLayout} size="40" />
         </p>
         <p>
           <textarea
@@ -46,8 +71,8 @@ export default function CopyLayout() {
           ></textarea>
         </p>
       </form>
-      <Button onClick={CopyLayout}>saveLayout</Button>
-      <Button onClick={getLayout}>Get Layout</Button>
+      <Button onClick={(CopyLayout, () => setArr([...arr, layout]))}>saveLayout</Button>
+      {/* <Button onClick={getLayout}>{nameLayout}</Button> */}
     </>
   );
 }
