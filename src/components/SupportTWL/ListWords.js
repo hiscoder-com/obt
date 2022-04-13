24b6cb8b59425651e7a4168d49f00b07ab46cd89
valyukhov <alexzed@bk.ru>
2022-04-13 21:24:07 +0300

    create list words

--- src/components/SupportTWL/ListWords.js
+++ src/components/SupportTWL/ListWords.js
@@ -1,35 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ListAltRoundedIcon from '@material-ui/icons/ListAltRounded';
 import { Box, Popover } from '@material-ui/core';
 
 function ListWords({ items, itemIndex, tsvs }) {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [listWords, setListWords] = useState();
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const list = [];
 
-  if (tsvs) {
-    Object.entries(tsvs).forEach(([key, chapters]) => {
-      Object.entries(chapters).forEach(([key, verses]) => {
-        verses.forEach((verse) => {
-          const el = verse.TWLink;
-          list.push({ el: verse.TWLink });
+  useEffect(() => {
+    const list = {};
+    if (tsvs) {
+      Object.entries(tsvs).forEach(([key, chapters]) => {
+        Object.entries(chapters).forEach(([key, verses]) => {
+          verses.forEach((verse) => {
+            if (Object.keys(list).includes(verse.TWLink)) {
+              if (list[verse.TWLink].includes(verse.Reference)) {
+                return;
+              } else {
+                list[verse.TWLink] = [...list[verse.TWLink], verse.Reference];
+              }
+            } else {
+              list[verse.TWLink] = [verse.Reference];
+            }
+          });
         });
       });
-    });
-  }
-  console.log(list);
+    }
+    setListWords(list);
+    console.log(list);
+  }, [tsvs]);
+
   return (
     <>
       <Box display={'flex'} justifyContent={'center'}>
-        <ListAltRoundedIcon
-          onClick={(event) => setAnchorEl(event.currentTarget)}
-          color="primary"
-        />
+        {items &&
+          items[itemIndex] &&
+          listWords &&
+          listWords[items[itemIndex].TWLink] &&
+          listWords[items[itemIndex].TWLink].length > 1 && (
+            <ListAltRoundedIcon
+              onClick={(event) => setAnchorEl(event.currentTarget)}
+              color="primary"
+            />
+          )}
       </Box>
       <Popover anchorEl={anchorEl} onClose={handleClose} open={Boolean(anchorEl)}>
-        {items && itemIndex && items[itemIndex].TWLink}
+        {items &&
+          itemIndex &&
+          listWords[items[itemIndex].TWLink].map((el, key) => {
+            return (
+              <p style={{ paddingLeft: '15px', paddingRight: '15px' }} key={key}>
+                {el}
+              </p>
+            );
+          })}
       </Popover>
     </>
   );
