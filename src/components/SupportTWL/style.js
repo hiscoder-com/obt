140833e9b15e17429d71b985506d903ff100cdd3
valyukhov <alexzed@bk.ru>
2022-04-29 17:10:27 +0300

    change links view

--- src/components/SupportTWL/style.js
+++ src/components/SupportTWL/style.js
@@ -2,7 +2,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   linkContainer: {
+    cursor: 'pointer',
     padding: theme.spacing(1 / 2, 2),
+    display: 'block',
   },
   twl: {
     color: theme.palette.text.disabled,
